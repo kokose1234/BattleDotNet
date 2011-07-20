12444178ba6ef91c97dddebe6104ddcfe43de556
Chad Moran <chad@chadmoran.com>
2011-07-19 21:16:58 -0500

    Setting stuff up

---
+++ BattleDotNet/Client.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BattleDotNet
+{
+    public abstract class Client
+    {
+        public Client()
+        {
+        }
+    }
+}
--- BattleDotNet/RequestManager.cs
+++ BattleDotNet/RequestManager.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net;
+using System.Threading;
+using System.IO;
 
 namespace BattleDotNet
 {
@@ -9,6 +12,35 @@ namespace BattleDotNet
     {
         public RequestManager()
         {
+        }
+
+        public string GetContent(string url)
+        {
+            HttpWebRequest webRequest = HttpWebRequest.Create(url) as HttpWebRequest;
+            
+            // Use GZip/Delfate when possible
+            webRequest.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+            
+            // This will help with speed by keeping the internal connection
+            // pool on a per-thread basis
+            webRequest.ConnectionGroupName = Thread.CurrentThread.ManagedThreadId.ToString();
+
+            // Will help with speed by not having to check for automatic settings
+            webRequest.Proxy = null;
+
+            // Give some information
+            webRequest.UserAgent = "BattleDotNet C# Library | http://github.com/ChadMoran/BattleDotNet";
+
+            using (HttpWebResponse webResponse = webRequest.GetResponse() as HttpWebResponse)
+            {
+                using (Stream responseStream = webResponse.GetResponseStream())
+                {
+                    using (StreamReader reader = new StreamReader(responseStream))
+                    {
+                        return reader.ReadToEnd();
+                    }
+                }
+            }
         }
     }
 }
