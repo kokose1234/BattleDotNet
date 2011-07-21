622cc6f4015abbfe9d6fb31bce0de93f259e0aad
Jeremy McPeak <jwmcpeak@wdonline.com>
2011-07-21 10:26:36 -0500

    Added more to RealmStatus. Also started Character and class info.

---
+++ BattleDotNet/Objects/WoW/Character.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace BattleDotNet.Objects.WoW
+{
+    public class Character
+    {
+        [JsonProperty("name")]
+        public string Name { get; private set; }
+
+        [JsonProperty("level")]
+        public int Level { get; private set; }
+    }
+}
