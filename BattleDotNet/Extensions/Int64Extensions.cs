4febe43b959ce2e1e65e6b65a9dfa0447a4702a2
jwmcpeak <jwmcpeak@gmail.com>
2011-07-24 00:03:41 -0500

    Added extension class for Int64; JavaScriptTimeStampConverter now uses it.

---
+++ BattleDotNet/Extensions/Int64Extensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BattleDotNet.Extensions
+{
+    public static class Int64Extensions
+    {
+        public static DateTime ConvertJavaScriptTimestamp(this long timestamp)
+        {
+            timestamp = timestamp / 1000;
+            DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0);
+
+            return epoch.AddSeconds(timestamp);
+        }
+    }
+}
