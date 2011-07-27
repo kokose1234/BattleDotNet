261d44f31920e2cc984b9f8c372ef6c0f2ae2cce
Jeremy McPeak <jwmcpeak@wdonline.com>
2011-07-27 08:05:52 -0500

    Updated guild achievements to mirror character achievements

---
+++ BattleDotNet/Objects/WoW/GuildAchievement.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BattleDotNet.Objects.WoW
+{
+    public class GuildAchievement
+    {
+        public GuildAchievement(int id, DateTime dateCompleted)
+        {
+            ID = id;
+            DateCompleted = dateCompleted;
+        }
+
+        public int ID { get; private set; }
+        public DateTime DateCompleted { get; private set; }
+    }
+}
