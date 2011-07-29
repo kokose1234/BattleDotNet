68c67cfb557a4324b256f48ba2c76d504f46753e
jwmcpeak <jwmcpeak@gmail.com>
2011-07-28 21:24:25 -0500

    Began Item API; it is incomplete

---
+++ BattleDotNet/Objects/WoW/ItemClassInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace BattleDotNet.Objects.WoW
+{
+    public struct ItemClassInfo
+    {
+        [JsonProperty("class")]
+        public int Class { get; private set; }
+
+        [JsonProperty("name")]
+        public string Name { get; private set; }
+
+        public ItemClassInfo() { }
+
+        public ItemClassInfo(int classId, string name)
+        {
+            Class = classId;
+            Name = name;
+        }
+    }
+}
---
+++ BattleDotNet/Objects/WoW/ItemSubClassInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace BattleDotNet.Objects.WoW
+{
+    public struct ItemSubClassInfo
+    {
+        [JsonProperty("subclassId")]
+        public int SubClassID { get; private set; }
+
+        [JsonProperty("name")]
+        public string Name { get; private set; }
+
+        public ItemSubClassInfo() { }
+
+        public ItemSubClassInfo(int classId, string name)
+        {
+            SubClassID = classId;
+            Name = name;
+        }
+    }
+}
