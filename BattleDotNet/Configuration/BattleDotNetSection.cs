95306d78d882b7bf2f6e8d1fc6bc274c29f30a85
jwmcpeak <jwmcpeak@gmail.com>
2011-07-29 07:11:18 -0500

    Began configuration handler

---
+++ BattleDotNet/Configuration/BattleDotNetSection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+
+namespace BattleDotNet.Configuration
+{
+    public class BattleDotNetSection : ConfigurationSection
+    {
+        /* Basic configuration layout
+         * 
+         * <battleDotNet>
+         *  <publicKey />
+         *  <privateKey />
+         * </battleDotNet>
+         * 
+         */ 
+
+
+    }
+}
