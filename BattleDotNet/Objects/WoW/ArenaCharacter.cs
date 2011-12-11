d2be4cf173a00616420f841957ce465d0204301c
Chad Moran <chad@chadmoran.com>
2011-12-11 11:01:22 -0800

    Missing file

---
+++ BattleDotNet/Objects/WoW/ArenaCharacter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace BattleDotNet.Objects.WoW
+{
+    public class ArenaCharacter
+    {
+        [JsonProperty("character")]
+        public MetaCharacter Character { get; private set; }
+
+        [JsonProperty("gamesLost")]
+        public int GamesLost { get; private set; }
+
+        [JsonProperty("gamesPlayed")]
+        public int GamesPlayed { get; private set; }
+
+        [JsonProperty("gamesWon")]
+        public int GamesWon { get; private set; }
+
+        [JsonProperty("rank")]
+        public int Rank { get; private set; }
+
+        [JsonProperty("personalRating")]
+        public int Rating { get; private set; }
+
+        [JsonProperty("sessionGamesLost")]
+        public int SessionGamesLost { get; private set; }
+
+        [JsonProperty("sessionGamesPlayed")]
+        public int SessionGamesPlayed { get; private set; }
+
+        [JsonProperty("sessionGamesWon")]
+        public int SessionGamesWon { get; private set; }
+    }
+}
---
+++ BattleDotNet/Objects/WoW/ArenaTeam.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace BattleDotNet.Objects.WoW
+{
+    public class ArenaTeam
+    {
+        [JsonProperty("created")]
+        public DateTime CreatedOn { get; private set; }
+
+        [JsonProperty("side")]
+        public Faction Faction { get; private set; }
+
+        [JsonProperty("gamesLost")]
+        public int GamesLost { get; private set; }
+
+        [JsonProperty("gamesPlayed")]
+        public int GamesPlayed { get; private set; }
+
+        [JsonProperty("gamesWon")]
+        public int GamesWon { get; private set; }
+
+        [JsonProperty("lastSessionRanking")]
+        public int LastSessionRanking { get; private set; }
+
+        [JsonProperty("members")]
+        public ArenaCharacter[] Members { get; private set; }
+
+        [JsonProperty("name")]
+        public string Name { get; private set; }
+
+        [JsonProperty("ranking")]
+        public int Ranking { get; private set; }
+
+        [JsonProperty("rating")]
+        public int Rating { get; private set; }
+
+        [JsonProperty("realm")]
+        public string Realm { get; private set; }
+
+        [JsonProperty("sessionGamesLost")]
+        public int SessionGamesLost { get; private set; }
+
+        [JsonProperty("sessionGamesPlayed")]
+        public int SessionGamesPlayed { get; private set; }
+
+        [JsonProperty("sessionGamesWon")]
+        public int SessionGamesWon { get; private set; }
+
+        [JsonProperty("teamsize")]
+        public int TeamSize { get; private set; }
+    }
+}
